dab46f70f0f2ce81b1658ed037a4d47271218c48
Michael Sornoza <michaelsornoza@Michaels-MacBook-Pro.local>
2023-01-15 08:21:01 -0500

    Vairables and Zero Values

--- src/main.go
+++ src/main.go
@@ -3,5 +3,36 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, World!")
+	// Constants
+
+	const pi float64 = 3.14
+	const pi2 = 3.14
+
+	fmt.Println("pi:", pi)
+	fmt.Println("pi2:", pi2)
+
+	// Int variables
+
+	base := 12
+	var height int = 14
+	var area int
+
+	area = base * height
+
+	fmt.Println("Area:", area)
+
+	// Zero Values
+	var a int
+	var b float64
+	var c string
+	var d bool
+
+	fmt.Println(a, b, c, d)
+
+	// Area de un cuadrado
+
+	const baseCuadrado = 10
+	areaCuadrado := baseCuadrado * baseCuadrado
+
+	fmt.Println("Area del cuadrado:", areaCuadrado)
 }
